2c718ede5eedc459bcd50b62aa9c76841c4a27ad
zhangzhenke <zzk18438111723@163.com>
2024-10-17 19:36:39 +0800

    转置卷积脚本

--- voc/utils.py
+++ voc/utils.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 from typing import Tuple, Type, List
-from SAM.image_encoder import ImageEncoderViT
 
 
 # 包含一个卷积层、批标准化和ReLU激活。
@@ -89,63 +88,3 @@ class Deconv2DBlock(nn.Module):
         return self.block(x)
 
 
-
-
-
-class ViTCellViTDeit(ImageEncoderViT):
-    def __init__(
-        self,
-        extract_layers: List[int],
-        img_size: int = 1024,
-        patch_size: int = 16,
-        in_chans: int = 3,
-        embed_dim: int = 768,
-        depth: int = 12,
-        num_heads: int = 12,
-        mlp_ratio: float = 4,
-        out_chans: int = 256,
-        qkv_bias: bool = True,
-        norm_layer: Type[nn.Module] = nn.LayerNorm,
-        act_layer: Type[nn.Module] = nn.GELU,
-        use_abs_pos: bool = True,
-        use_rel_pos: bool = False,
-        rel_pos_zero_init: bool = True,
-        window_size: int = 0,
-        global_attn_indexes: Tuple[int, ...] = (),
-    ) -> None:
-        super().__init__(
-            img_size,
-            patch_size,
-            in_chans,
-            embed_dim,
-            depth,
-            num_heads,
-            mlp_ratio,
-            out_chans,
-            qkv_bias,
-            norm_layer,
-            act_layer,
-            use_abs_pos,
-            use_rel_pos,
-            rel_pos_zero_init,
-            window_size,
-            global_attn_indexes,
-        )
-        self.extract_layers = extract_layers
-
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        extracted_layers = []
-        x = self.patch_embed(x)
-
-        if self.pos_embed is not None:
-            token_size = x.shape[1]
-            x = x + self.pos_embed[:, :token_size, :token_size, :]
-
-        for depth, blk in enumerate(self.blocks):
-            x = blk(x)
-            if depth + 1 in self.extract_layers:
-                extracted_layers.append(x)
-
-
-        return extracted_layers
