b106e60b5e21c7fe9be2c0d0a9e876a1f6ddfaa2
zhangzhenke <zzk18438111723@163.com>
2024-10-17 19:31:24 +0800

    卷积与转置卷积

---
+++ voc/utils.py
@@ -0,0 +1,151 @@
+import torch
+import torch.nn as nn
+
+from typing import Tuple, Type, List
+from SAM.image_encoder import ImageEncoderViT
+
+
+# 包含一个卷积层、批标准化和ReLU激活。
+class Conv2DBlock(nn.Module):
+    """
+    Conv2DBlock with convolution followed by batch-normalisation, ReLU activation and dropout
+
+    Args:
+        in_channels (int): Number of input channels for convolution
+        out_channels (int): Number of output channels for convolution
+        kernel_size (int, optional): Kernel size for convolution. Defaults to 3.
+        dropout (float, optional): Dropout. Defaults to 0.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        dropout: float = 0,
+    ) -> None:
+        super().__init__()
+        
+        self.block = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=((kernel_size - 1) // 2),
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+# 包含一个转置卷积层、卷积层、批标准化和ReLU激活。
+class Deconv2DBlock(nn.Module):
+    """
+    Deconvolution block with ConvTranspose2d followed by Conv2d, batch-normalisation, ReLU activation and dropout
+
+    Args:
+        in_channels (int): Number of input channels for deconv block
+        out_channels (int): Number of output channels for deconv and convolution.
+        kernel_size (int, optional): Kernel size for convolution. Defaults to 3.
+        dropout (float, optional): Dropout. Defaults to 0.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int = 3,
+        dropout: float = 0,
+    ) -> None:
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=2,
+                stride=2,
+                padding=0,
+                output_padding=0,
+            ),
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=((kernel_size - 1) // 2),
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+
+
+
+class ViTCellViTDeit(ImageEncoderViT):
+    def __init__(
+        self,
+        extract_layers: List[int],
+        img_size: int = 1024,
+        patch_size: int = 16,
+        in_chans: int = 3,
+        embed_dim: int = 768,
+        depth: int = 12,
+        num_heads: int = 12,
+        mlp_ratio: float = 4,
+        out_chans: int = 256,
+        qkv_bias: bool = True,
+        norm_layer: Type[nn.Module] = nn.LayerNorm,
+        act_layer: Type[nn.Module] = nn.GELU,
+        use_abs_pos: bool = True,
+        use_rel_pos: bool = False,
+        rel_pos_zero_init: bool = True,
+        window_size: int = 0,
+        global_attn_indexes: Tuple[int, ...] = (),
+    ) -> None:
+        super().__init__(
+            img_size,
+            patch_size,
+            in_chans,
+            embed_dim,
+            depth,
+            num_heads,
+            mlp_ratio,
+            out_chans,
+            qkv_bias,
+            norm_layer,
+            act_layer,
+            use_abs_pos,
+            use_rel_pos,
+            rel_pos_zero_init,
+            window_size,
+            global_attn_indexes,
+        )
+        self.extract_layers = extract_layers
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        extracted_layers = []
+        x = self.patch_embed(x)
+
+        if self.pos_embed is not None:
+            token_size = x.shape[1]
+            x = x + self.pos_embed[:, :token_size, :token_size, :]
+
+        for depth, blk in enumerate(self.blocks):
+            x = blk(x)
+            if depth + 1 in self.extract_layers:
+                extracted_layers.append(x)
+
+
+        return extracted_layers
